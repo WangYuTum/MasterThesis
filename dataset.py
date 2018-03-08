3e91ab300831607d8c69a546d19fc0d1d902ca89
Yu Wang <wangyu@in.tum.de>
2018-03-08 17:16:33 +0100

    Update data preprocessing notebook. Compute mean,std on all train/val/test-dev/test-challenge images. Generate train/val list txt file.

--- dataset.py
+++ dataset.py
@@ -1,2 +1,7 @@
-# Mean R:115.412473189, G:115.199299201, B:108.512946611
-# Std R:64.3412719996, G:62.1317242839, B:66.9034415066
+## All trainnig images are resized to 854x480 (only a few). All val/test images remain the same.
+## Means and Stds are computed on tran, val, test-dev, test-challenge sets including 2016 & 2017 datasets
+## Mean R:115.195829334, G:114.927476686, B:107.725750308
+## Std R:64.5572961827, G:63.0172054007, B:67.0494050908
+
+## 4209 train frames. 1999 val frames.
+## 2086 test_dev frames. 2180 test_challenge frames.
