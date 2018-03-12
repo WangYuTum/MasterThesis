5a9962a8c686697a7a55674f2eb90ace90bb6c9a
Yu Wang <wangyu@in.tum.de>
2018-03-12 18:20:39 +0100

    Main structure of network completed.

--- core/nn.py
+++ core/nn.py
@@ -8,6 +8,8 @@ from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
+import sys
+
 
 def conv_layer(data_format, input_tensor, stride=1, padding='SAME', shape=None):
     ''' The standard convolution layer '''
@@ -24,9 +26,11 @@ def conv_layer(data_format, input_tensor, stride=1, padding='SAME', shape=None):
 
     return conv_out
 
+
 def res_side(data_format, input_tensor, shape_dict, is_train=False):
     ''' The residual block unit with side conv '''
 
+    # The 1st bn layer
     BN_out1 = BN(data_format, input_tensor, 'bn1', shape_dict['convs'][0][2], is_train)
     RELU_out1 = ReLu_layer(BN_out1)
 
@@ -34,11 +38,48 @@ def res_side(data_format, input_tensor, shape_dict, is_train=False):
     with tf.variable_scope('side'):
         side_out = conv_layer(data_format, RELU_out1, 1, 'SAME', shape_dict['side'])
     # The 1st conv
+    with tf.variable_scope('conv1'):
+        CONV_out1 = conv_layer(data_format, RELU_out1, 1, 'SAME', shape_dict['convs'][0])
+    # The 2nd bn layer
+    BN_out2 = BN(data_format, CONV_out1, 'bn2', shape_dict['convs'][1][2], is_train)
+    RELU_out2 = ReLu_layer(BN_out2)
+    # The 2nd conv layer
+    with tf.variable_scope('conv2'):
+        CONV_out2 = conv_layer(data_format, RELU_out2, 1, 'SAME', shape_dict['convs'][1])
+    # Fuse
+    RES_out = tf.add(side_out, CONV_out2)
+
+    return  RES_out
+
 
 def res(data_format, input_tensor, shape_dict, is_train=False):
     ''' The residual block unit with shortcut '''
 
-    # TODO
+    scope_name = tf.get_variable_scope().name
+    if scope_name.find('B4') != -1:
+        shape_conv1 = shape_dict[0]
+        shape_conv2 = shape_dict[1]
+    else:
+        shape_conv1 = shape_dict[1]
+        shape_conv2 = shape_dict[1]
+
+    # The 1st bn layer
+    BN_out1 = BN(data_format, input_tensor, 'bn1', shape_conv1[2], is_train)
+    RELU_out1 = ReLu_layer(BN_out1)
+    # The 1st conv layer
+    with tf.variable_scope('conv1'):
+        CONV_out1 = conv_layer(data_format, RELU_out1, 1, 'SAME', shape_conv1)
+    # The 2nd bn layer
+    BN_out2 = BN(data_format, CONV_out1, 'bn2', shape_conv2[2], is_train)
+    RELU_out2 = ReLu_layer(BN_out2)
+    # The 2nd conv layer
+    with tf.variable_scope('conv2'):
+        CONV_out2 = conv_layer(data_format, RELU_out2, 1, 'SAME', shape_conv2)
+    # Fuse
+    RES_out = tf.add(input_tensor, CONV_out2)
+
+    return RES_out
+
 
 def create_conv_kernel(shape=None):
     '''
@@ -50,6 +91,7 @@ def create_conv_kernel(shape=None):
     var = tf.get_variable(name='kernel', shape=shape, initializer=init_op)
 
     return var
+
 
 def max_pool2d(data_format, input_tensor, stride=2, padding='SAME'):
     ''' The standard max_pool2d with kernel size 2x2 '''
@@ -64,6 +106,7 @@ def max_pool2d(data_format, input_tensor, stride=2, padding='SAME'):
     out = tf.nn.max_pool(input_tensor, pool_size, pool_stride, padding, data_format)
 
     return out
+
 
 def BN(data_format, input_tensor, bn_scope=None, shape=None, is_train=False):
 
@@ -125,6 +168,94 @@ def ReLu_layer(input_tensor):
     relu_out = tf.nn.relu(input_tensor)
 
     return relu_out
+
+
+def get_conv_transpose_ksize(factor):
+    # Input: specify upsampling factor
+    return 2 * factor - factor % 2
+
+
+def upsample_filt(size):
+    """
+    Make a 2D bilinear kernel suitable for upsampling of the given (h, w) kernel size.
+    """
+    factor = (size + 1) // 2
+    if size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:size, :size]
+    return (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
+
+
+def set_conv_transpose_filters(variables):
+    # Input: all tf.Variables
+    set_filter_ops = []
+    for v in variables:
+        if '-up' in v.name:
+            h, w, k, m = v.get_shape()
+            tmp = np.zeros((m, k, h, w))
+            if m != k:
+                sys.exit("Transpose kernel input and output channels must be the same.")
+            if h != w:
+                sys.exit("Transpose kernel height and width must be the same. ")
+            up_filter = upsample_filt(int(h))
+            tmp[range(m), range(k), :, :] = up_filter
+            set_filter_ops.append(tf.assign(v, tmp.transpose(2, 3, 1, 0)))
+
+    return set_filter_ops
+
+
+def conv_transpose(data_format, input_tensor, factor=0, padding='SAME'):
+    # Create a transposed conv layer according to upsampling factor.
+    # This function must be put into an appropriate variable scope.
+    # All transposed filters must be reset to bilinear after tf.train.Saver store the variables
+
+    batch_size = tf.shape(input_tensor)[0]
+    trans_ksize = get_conv_transpose_ksize(factor)
+    init_filter = tf.get_variable('bilinear-up',
+                                  shape=[trans_ksize, trans_ksize, 16, 16],
+                                  initializer = tf.zeros_initializer(),
+                                  trainable = False)
+    if data_format == "NCHW":
+        out_channels = tf.shape(input_tensor)[1]
+        new_H = tf.shape(input_tensor)[2]
+        new_W = tf.shape(input_tensor)[3]
+        trans_output_shape = [batch_size, out_channels, new_H, new_W]
+        trans_stride = [1, 1, factor, factor]
+    else:
+        out_channels = tf.shape(input_tensor)[3]
+        new_H = tf.shape(input_tensor)[1]
+        new_W = tf.shape(input_tensor)[2]
+        trans_output_shape = [batch_size, new_H, new_W, out_channels]
+        trans_stride = [1, factor, factor, 1]
+
+    trans_out = tf.nn.conv2d_transpose(input_tensor,
+                                       init_filter,
+                                       trans_output_shape,
+                                       trans_stride,
+                                       padding,
+                                       data_format)
+
+    return trans_out
+
+def crop_features(data_format, feature, out_size):
+    # Slice down the feature to out_size
+    # This is done after deconv since deconv output may exceed original image size
+
+    up_size = tf.shape(feature)
+    if data_format == "NCHW":
+        ini_h = tf.div(tf.subtract(up_size[2], out_size[2]), 2) # might be zero
+        ini_w = tf.div(tf.subtract(up_size[3], out_size[3]), 2) # might be zero
+        slice_input = tf.slice(feature, (0, 0, ini_h, ini_w), (-1, -1, out_size[2], out_size[3]))
+        return tf.reshape(slice_input, [int(feature.get_shape()[0]), int(feature.get_shape()[1]), out_size[2], out_size[3]])
+    else:
+        ini_h = tf.div(tf.subtract(up_size[1], out_size[1]), 2)  # might be zero
+        ini_w = tf.div(tf.subtract(up_size[2], out_size[2]), 2)  # might be zero
+        slice_input = tf.slice(feature, (0, ini_h, ini_w, 0), (-1, out_size[1], out_size[2], -1))
+        return tf.reshape(slice_input, [int(feature.get_shape()[0]), out_size[1], out_size[2], int(feature.get_shape()[3])])
+
 
 
 
--- core/resnet.py
+++ core/resnet.py
@@ -3,10 +3,14 @@
 
     NOTE: the code is still under developing.
     TODO:
+        ** padding in deconv layer ??? SAME-osvos, VALID-online_tutorial
+        ** bias layers ??? must add on side path. on backbone net?
+        ** side supervision loss. read InceptionNet
         * load pre-trained ImageNet ckpt. Pay attention to RGB means.
         * Saver for save/restore weights. Check for each layer whether first created or restored.
-        * Estimator ...
+        * Learning rate scheduler
         * Apply diff lr for diff layers
+        * Estimator ...
 '''
 
 from __future__ import division
@@ -53,20 +57,115 @@ class ResNet():
         shape_dict['B0'] = [3,3,3,64]
 
         with tf.variable_scope('main'):
-            # Residual Block B0 (x1)
+            # Residual Block B0
             with tf.variable_scope('B0'):
                 model['B0'] = nn.conv_layer(self._data_format, images, 1, 'SAME', shape_dict['B0'])
-                model['B0_pooled'] = nn.max_pool2d(self._data_format, model['B0'], 2, 'SAME')
 
-            # Residual Block B1_1, B1_2, B1_3
+            # Pooling 1
+            model['B0_pooled'] = nn.max_pool2d(self._data_format, model['B0'], 2, 'SAME')
+
+            # Residual Block B1_0, B1_1, B1_2
             shape_dict['B1'] = {}
-            shape_dict['B1']['side'] = [1,1,64,128]
-            shape_dict['B1']['convs'] = [[3,3,64,128],[3,3,128,128]]
+            shape_dict['B1']['side'] = [1, 1, 64, 128]
+            shape_dict['B1']['convs'] = [[3, 3, 64, 128], [3, 3, 128, 128]]
             with tf.variable_scope('B1_0'):
-                model['B1_1'] = nn.res_side()
+                model['B1_0'] = nn.res_side(self._data_format, model['B0_pooled'], shape_dict['B1'], is_train)
             for i in range(2):
-                with tf.variable_scope('B1_'+str(i+1)):
-                    model['B1_'+str(i+1)] = nn.res()
+                with tf.variable_scope('B1_' + str(i + 1)):
+                    model['B1_' + str(i + 1)] = nn.res(self._data_format, model['B1_' + str(i)],
+                                                   shape_dict['B1']['convs'], is_train)
+
+            # Pooling 2
+            model['B1_2_pooled'] = nn.max_pool2d(self._data_format, model['B1_2'], 2, 'SAME')
+
+            # Residual Block B2_0, B2_1, B2_2
+            shape_dict['B2'] = {}
+            shape_dict['B2']['side'] = [1, 1, 128, 256]
+            shape_dict['B2']['convs'] = [[3, 3, 128, 256], [3, 3, 256, 256]]
+            with tf.variable_scope('B2_0'):
+                model['B2_0'] = nn.res_side(self._data_format, model['B1_2_pooled'], shape_dict['B2'], is_train)
+            for i in range(2):
+                with tf.variable_scope('B2_' + str(i + 1)):
+                    model['B2_' + str(i + 1)] = nn.res(self._data_format, model['B2_' + str(i)],
+                                                       shape_dict['B2']['convs'], is_train)
+
+            # Pooling 3
+            model['B2_2_pooled'] = nn.max_pool2d(self._data_format, model['B2_2'], 2, 'SAME')
+
+            # Residual Block B3_0 - B3_5
+            shape_dict['B3'] = {}
+            shape_dict['B3']['side'] = [1, 1, 256, 512]
+            shape_dict['B3']['convs'] = [[3, 3, 512, 512], [3, 3, 512, 512]]
+            with tf.variable_scope('B3_0'):
+                model['B3_0'] = nn.res_side(self._data_format, model['B2_2_pooled'], shape_dict['B3'], is_train)
+            for i in range(5):
+                with tf.variable_scope('B3_' + str(i + 1)):
+                    model['B3_' + str(i + 1)] = nn.res(self._data_format, model['B3_' + str(i)],
+                                                       shape_dict['B3']['convs'], is_train)
+
+            # Pooling 4
+            model['B3_5_pooled'] = nn.max_pool2d(self._data_format, model['B3_5'], 2, 'SAME')
+
+            # Residual Block B4_0, B4_1, B4_2
+            shape_dict['B4_0'] = {}
+            shape_dict['B4_0']['side'] = [1, 1, 512, 1024]
+            shape_dict['B4_0']['convs'] = [[3, 3, 512, 512],[3, 3, 512, 1024]]
+            with tf.variable_scope('B4_0'):
+                model['B4_0'] = nn.res_side(self._data_format, model['B3_5_pooled'], shape_dict['B4_0'], is_train)
+            shape_dict['B4_23'] = [[3, 3, 1024, 512], [3, 3, 512, 1024]]
+            for i in range(2):
+                with tf.variable_scope('B4_' + str(i + 1)):
+                    model['B4_' + str(i + 1)] = nn.res(self._data_format, model['B4_' + str(i)],
+                                                       shape_dict['B4_23'], is_train)
+
+            # add side conv path and upsample, crop to image size
+            im_size = tf.shape(images)
+            with tf.variable_scope('B1_side_path'):
+                side_2 = nn.conv_layer(self._data_format, model['B1_2'], 1, 'SAME', [3, 3, 128, 16])
+                side_2_f = nn.conv_transpose(self._data_format, side_2, 2, 'SAME')
+                side_2_f = nn.crop_features(self._data_format, side_2_f, im_size)
+            with tf.variable_scope('B2_side_path'):
+                side_4 = nn.conv_layer(self._data_format, model['B2_2'], 1, 'SAME', [3, 3, 256, 16])
+                side_4_f = nn.conv_transpose(self._data_format, side_4, 4, 'SAME')
+                side_4_f = nn.crop_features(self._data_format, side_4_f, im_size)
+            with tf.variable_scope('B3_side_path'):
+                side_8 = nn.conv_layer(self._data_format, model['B3_5'], 1, 'SAME', [3, 3, 512, 16])
+                side_8_f = nn.conv_transpose(self._data_format, side_8, 8, 'SAME')
+                side_8_f = nn.crop_features(self._data_format, side_8_f, im_size)
+            with tf.variable_scope('B4_side_path'):
+                side_16 = nn.conv_layer(self._data_format, model['B4_2'], 1, 'SAME', [3, 3, 1024, 16])
+                side_16_f = nn.conv_transpose(self._data_format, side_16, 16, 'SAME')
+                side_16_f = nn.crop_features(self._data_format, side_16_f, im_size)
+
+            # concat and linearly fuse
+            if self._data_format == "NCHW":
+                concat_side = tf.concat([side_2_f, side_4_f, side_8_f, side_16_f], axis=1)
+            else:
+                concat_side = tf.concat([side_2_f, side_4_f, side_8_f, side_16_f], axis=3)
+            with tf.variable_scope('fuse'):
+                net_out = nn.conv_layer(self._data_format, concat_side, 1, 'SAME', [1, 1, 64, 1])
+
+        return net_out
+
+    def train(self, images, gts, params):
+
+
+        total_loss = None
+
+        return total_loss
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
