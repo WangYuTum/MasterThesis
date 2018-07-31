9207ccff9cfeb66530419da6ced61565d991ecde
Yu Wang <wangyu@in.tum.de>
2018-07-31 17:47:13 +0200

    Test for v4_large 80ep finished.

--- run/test_parent_bin.py
+++ run/test_parent_bin.py
@@ -11,6 +11,59 @@ from core import resnet
 from scipy.misc import imsave
 from scipy.ndimage.morphology import binary_dilation
 from scipy.ndimage import generate_binary_structure
+import glob
+from PIL import Image
+TAG_FLOAT = 202021.25
+
+def read_flow(file):
+
+    assert type(file) is str, "file is not str %r" % str(file)
+    assert os.path.isfile(file) is True, "file does not exist %r" % str(file)
+    assert file[-4:] == '.flo', "file ending is not .flo %r" % file[-4:]
+    f = open(file,'rb')
+    flo_number = np.fromfile(f, np.float32, count=1)[0]
+    assert flo_number == TAG_FLOAT, 'Flow number %r incorrect. Invalid .flo file' % flo_number
+    w = np.fromfile(f, np.int32, count=1)
+    h = np.fromfile(f, np.int32, count=1)
+    #if error try: data = np.fromfile(f, np.float32, count=2*w[0]*h[0])
+    data = np.fromfile(f, np.float32, count=2*w*h)
+    # Reshape data into 3D array (columns, rows, bands)
+    flow = np.resize(data, (int(h), int(w), 2))
+    f.close()
+
+    return flow
+
+def transf_slow(seg_arr, flow_arr):
+    seg_shape = seg_arr.shape
+    flow_shape = flow_arr.shape
+    assert seg_shape[0] == flow_shape[0], "Seg/Flow shape does not match!"
+    assert seg_shape[1] == flow_shape[1], "Seg/Flow shape does not match!"
+    h = seg_shape[0]
+    w = seg_shape[1]
+    new_seg = np.zeros((h, w), np.uint8)
+    for idx_h in range(h):
+        for idx_w in range(w):
+            if seg_arr[idx_h][idx_w] == 1:
+                motion_h = int(round(flow_arr[idx_h][idx_w][1]))
+                motion_w = int(round(flow_arr[idx_h][idx_w][0]))
+                new_h = idx_h + motion_h
+                new_w = idx_w + motion_w
+                if new_h < h and new_h >= 0 and new_w < w and new_w >= 0:
+                    new_seg[new_h][new_w] = 1
+
+    return new_seg
+
+def overlay_seg_rgb(seg_arr, rgb_obj):
+
+    seg_arr = seg_arr.astype(np.uint8)
+    img_palette = np.array([0, 0, 0, 150, 0, 0])
+    seg_color = Image.fromarray(seg_arr, mode='P')
+    seg_color.putpalette(img_palette)
+    overlay_mask = Image.fromarray(seg_arr * 150, mode='L')
+
+    com_img = Image.composite(seg_color, rgb_obj, overlay_mask)
+
+    return com_img
 
 # parse argument
 arg_fine_tune = int(sys.argv[1])
@@ -74,7 +127,7 @@ if FINE_TUNE == 1:
         'restore_parent_bin': '../data/ckpts/attention_bin/CNN-part-gate-img-v4_large/att_bin.ckpt-48000'
     }
     global_iters = 1000 # original paper: 500
-    save_ckpt_interval = 100
+    save_ckpt_interval = [24300, 24500, 24700, 25000]
     summary_write_interval = 10
     print_screen_interval = 10
     acc_count = 1
@@ -84,8 +137,8 @@ else:
     params_model = {
         'batch': 1,
         'data_format': 'NCHW',  # optimal for cudnn
-        'restore_fine-tune_bin': '../data/ckpts/fine-tune/attention_bin/CNN-part-gate-img-v4_large/80ep/'+val_seq_paths[FINE_TUNE_seq].split('/')[-1]+'/fine-tune.ckpt-48700',
-        'save_result_path': '../data/results/iter1000/'+val_seq_paths[FINE_TUNE_seq].split('/')[-1]
+        'restore_fine-tune_bin': '../data/ckpts/fine-tune/attention_bin/CNN-part-gate-img-v4_large/80ep/'+val_seq_paths[FINE_TUNE_seq].split('/')[-1]+'/fine-tune.ckpt-48500',
+        'save_result_path': '../data/results/flow_att_seg/'+val_seq_paths[FINE_TUNE_seq].split('/')[-1]
     }
 
 # display on tsboard only during fine-tuning
@@ -152,7 +205,7 @@ with tf.Session(config=config_gpu) as sess:
                 sum_writer.add_summary(sum_all_, global_step.eval())
             if global_step.eval() % print_screen_interval == 0:
                 print("Fine-tune step {0} loss: {1}".format(global_step.eval(), loss_))
-            if global_step.eval() % save_ckpt_interval == 0 and global_step.eval() != 0:
+            if global_step.eval() in save_ckpt_interval and global_step.eval() != 0:
                 saver_fine_tune.save(sess=sess,
                                      save_path=params_model['save_path'],
                                      global_step=global_step,
@@ -162,17 +215,53 @@ with tf.Session(config=config_gpu) as sess:
 
     else:
         print("Starting inference for {}".format(val_seq_paths[FINE_TUNE_seq].split('/')[-1]))
+        ### Get optical flow for the sequence
+        flow_search = os.path.join(val_seq_paths[FINE_TUNE_seq].replace('JPEGImages', 'Flow'), '*.flo')
+        flow_files = glob.glob(flow_search)
+        flow_files.sort()
+        seg0_file = os.path.join(val_seq_paths[FINE_TUNE_seq].replace('JPEGImages', 'Annotations'), '00000.png')
+        seg0_arr = np.greater(np.array(Image.open(seg0_file)), 0).astype(np.uint8)
+        flow0 = read_flow(flow_files[0])
+        new_seg = transf_slow(seg0_arr, flow0)
+        struct1 = generate_binary_structure(2, 2)
+        att_flow = binary_dilation(new_seg, structure=struct1, iterations=30).astype(new_seg.dtype)
+        ###
+        ### Get image lists of the current sequence
+        img_search = os.path.join(val_seq_paths[FINE_TUNE_seq], '*.jpg')
+        img_files = glob.glob(img_search)
+        img_files.sort()
+        att_rgb_overlay_path = params_model['save_result_path'].replace('flow_att_seg', 'overlaid/flow-to-att_rgb')
+        seg_overlay_path = att_rgb_overlay_path.replace('flow-to-att_rgb', 'flow-to-att-to-seg_rgb')
+        ###
         for test_idx in range(len(test_frames)):
             print("Inference on frame {}".format(test_idx+1))
+            ### Save attention area on current image
+            rgb_obj = Image.open(img_files[test_idx+1])
+            att_rgb = overlay_seg_rgb(att_flow, rgb_obj)
+            att_rgb_save_name = str(test_idx+1).zfill(5) + '.png'
+            att_rgb_save_path = att_rgb_overlay_path + '/' + att_rgb_save_name
+            att_rgb.save(att_rgb_save_path)
+            ###
             feed_dict_v = {feed_img: test_frames[test_idx][0][np.newaxis,:],
-                           feed_att: test_frames[test_idx][1][np.newaxis,:]}
+                           feed_att: att_flow[np.newaxis,...,np.newaxis]}
             # prob_map: [1,H,W] tf.float32, mask: [1,H,W] tf.int16
             prob_map_, mask_ = sess.run([prob_map, mask], feed_dict=feed_dict_v)
             save_name = str(test_idx+1).zfill(5) + '.png'
             save_path = params_model['save_result_path'] + '/' + save_name
             # save binary mask
-            final_seg = np.multiply(np.squeeze(test_frames[test_idx][1]), np.squeeze(mask_))
+            final_seg = np.multiply(np.squeeze(att_flow), np.squeeze(mask_))
             imsave(save_path, final_seg)
+            ### Save seg overlay rgb
+            save_seg_rgb_path = seg_overlay_path + '/' + save_name
+            seg_rgb = overlay_seg_rgb(final_seg, rgb_obj)
+            seg_rgb.save(save_seg_rgb_path)
+            ###
+            ### Get next attention
+            if test_idx != len(test_frames) - 1:
+                flow_arr = read_flow(flow_files[test_idx + 1])
+                new_seg = transf_slow(final_seg, flow_arr)
+                att_flow = binary_dilation(new_seg, structure=struct1, iterations=35).astype(new_seg.dtype)
+            ###
             # save prob map
             # imsave(save_path.replace('.png', '_prob.png'), np.squeeze(prob_map_))
             print("Saved result.")
